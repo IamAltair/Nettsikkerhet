8dfd21063275ad0de806699d5f746cf2eee1b524
IamAltair <hpmathias@gmail.com>
2019-08-27 11:33:16 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/Nettsikkerhet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nettsikkerhet.dir/o1/hello.cpp.o"
+  "Nettsikkerhet.pdb"
+  "Nettsikkerhet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nettsikkerhet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
